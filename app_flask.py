f66c0cc85a356ee501965122b7aa2bde5e8cb670
mohamed emad <mohamed.emad4bubble@gmail.com>
2017-08-12 10:12:24 +0200

    Update app_flask.py

--- app_flask.py
+++ app_flask.py
@@ -3,8 +3,8 @@ from flask import Flask,url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return url_for('.', filename='index.html')
+@app.route('/<staticfile>',default={'staticfile':'index.html'})
+def hello_world(staticfile):
+   return url_for('.', filename=staticfile)
 
 app.run(port=int(os.environ.get('PORT')),host='0.0.0.0')
