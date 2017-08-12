cd1589762a712ad527302f1e6d7857ebf42cc9c5
mohamed emad <mohamed.emad4bubble@gmail.com>
2017-08-12 10:33:01 +0200

    Update app_bottle.py

--- app_bottle.py
+++ app_bottle.py
@@ -1,9 +1,12 @@
 import os
 import bottle
+
 @bottle.route("/<staticfile>")
-@bottle.route("/")
 def hello_world(staticfile):
-    if staticfile==None:
-       staticfile='index.html' 
     return bottle.static_file(root='.',filename=staticfile)
+
+@bottle.route('/')
+def re():
+    bottle.redirect('/index.html')
+    
 bottle.run(port=int(os.environ.get('PORT')),host='0.0.0.0')
