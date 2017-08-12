b3e24c11ebf22e9e060ba83ef8142e2595ab3fce
mohamed emad <mohamed.emad4bubble@gmail.com>
2017-08-12 09:37:42 +0200

    Update app_flask.py

--- app_flask.py
+++ app_flask.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask,url_for
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return '<h1>Hello World</h1>'
+   return url_for('.', filename='index.html')
+
 app.run(port=int(os.environ.get('PORT')),host='0.0.0.0')
