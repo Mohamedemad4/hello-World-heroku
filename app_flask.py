fb38b77a8f98c049c7538a981f11e96686882dd0
mohamed emad <mohamed.emad4bubble@gmail.com>
2017-08-12 10:23:39 +0200

    Update app_flask.py

--- app_flask.py
+++ app_flask.py
@@ -1,10 +1,14 @@
 import os
-from flask import Flask,url_for
+from flask import Flask,redirect
 
 app = Flask(__name__)
 
-@app.route('/<staticfile>',default={'staticfile':'index.html'})
+@app.route('/<staticfile>')
 def hello_world(staticfile):
-   return url_for('.', filename=staticfile)
+   return open(staticfile,'rb').read()
 
+@app.route('/')
+def re():
+    return redirect('/index.html')
+  
 app.run(port=int(os.environ.get('PORT')),host='0.0.0.0')
