2b281ae9eae36acaad415b0587f73e68ae400949
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2019-04-24 18:14:48 +0200

    ss

--- app_flask.py
+++ app_flask.py
@@ -1,14 +1,14 @@
 import os
-from flask import Flask,redirect
+from flask import Flask,redirect,request
 
 app = Flask(__name__)
 
-@app.route('/<staticfile>')
-def hello_world(staticfile):
-   return open(staticfile,'rb').read()
+#@app.route('/<staticfile>')
+#def hello_world(staticfile):
+#   return open(staticfile,'rb').read()
 
 @app.route('/')
 def re():
-    return redirect('/index.html')
+    return "404 Not found: {0}".format(request.remote_addr)  #redirect('/index.html')
   
 app.run(port=int(os.environ.get('PORT')),host='0.0.0.0')
